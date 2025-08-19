322632e7aa2a380c079706a40df47a810b1df667
nguynthuhigh <22dh114648@st.huflit.edu.vn>
2025-08-20 02:12:30 +0700

    feat: get range

--- crm-backend/src/utils/get-range-date.ts
+++ crm-backend/src/utils/get-range-date.ts
@@ -1,1 +1,42 @@
-export const getRangeDate = (range: string) => {};
+type RangeType = '1d' | '7d' | '1m' | '1y' | 'all';
+
+interface RangeResult {
+  fromDate: Date;
+  toDate: Date;
+  values: number;
+}
+
+export function getDateRange(range: RangeType): RangeResult {
+  const now = new Date();
+  let fromDate = new Date();
+  let values = 0;
+
+  switch (range) {
+    case '1d':
+      fromDate.setDate(now.getDate() - 1);
+      values = 6;
+      break;
+
+    case '7d':
+      fromDate.setDate(now.getDate() - 7);
+      values = 7;
+      break;
+
+    case '1m':
+      fromDate.setMonth(now.getMonth() - 1);
+      values = 30;
+      break;
+
+    case '1y':
+      fromDate.setFullYear(now.getFullYear() - 1);
+      values = 12;
+      break;
+
+    case 'all':
+      fromDate = new Date(1970, 0, 1);
+      values = 5;
+      break;
+  }
+
+  return { fromDate, toDate: now, values };
+}
